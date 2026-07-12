721d7e7a902a70d10176ddc04032e2348a6770b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "arc_private_link_scopes" {
-  description = "All arc_private_link_scope resources"
-  value       = azurerm_arc_private_link_scope.arc_private_link_scopes
-}
 output "arc_private_link_scopes_location" {
-  description = "List of location values across all arc_private_link_scopes"
-  value       = [for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : v.location]
+  description = "Map of location values across all arc_private_link_scopes, keyed the same as var.arc_private_link_scopes"
+  value       = { for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : k => v.location }
 }
 output "arc_private_link_scopes_name" {
-  description = "List of name values across all arc_private_link_scopes"
-  value       = [for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : v.name]
+  description = "Map of name values across all arc_private_link_scopes, keyed the same as var.arc_private_link_scopes"
+  value       = { for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : k => v.name }
 }
 output "arc_private_link_scopes_public_network_access_enabled" {
-  description = "List of public_network_access_enabled values across all arc_private_link_scopes"
-  value       = [for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : v.public_network_access_enabled]
+  description = "Map of public_network_access_enabled values across all arc_private_link_scopes, keyed the same as var.arc_private_link_scopes"
+  value       = { for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : k => v.public_network_access_enabled }
 }
 output "arc_private_link_scopes_resource_group_name" {
-  description = "List of resource_group_name values across all arc_private_link_scopes"
-  value       = [for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : v.resource_group_name]
+  description = "Map of resource_group_name values across all arc_private_link_scopes, keyed the same as var.arc_private_link_scopes"
+  value       = { for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : k => v.resource_group_name }
 }
 output "arc_private_link_scopes_tags" {
-  description = "List of tags values across all arc_private_link_scopes"
-  value       = [for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : v.tags]
+  description = "Map of tags values across all arc_private_link_scopes, keyed the same as var.arc_private_link_scopes"
+  value       = { for k, v in azurerm_arc_private_link_scope.arc_private_link_scopes : k => v.tags }
 }
 
